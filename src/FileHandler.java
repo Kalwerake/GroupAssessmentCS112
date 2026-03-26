e89c44fb72bc2465d051710a2d6455f79f917c34
Kalwane Werake <35309973+Kalwerake@users.noreply.github.com>
2026-03-26 13:55:42 +0000

    file handler methods done (#2)
    Co-authored-by: Kalwane Werake (Student) <kalwane.werake.2025@uni.strath.ac.uk>

--- src/FileHandler.java
+++ src/FileHandler.java
@@ -1,2 +1,41 @@
+import java.util.ArrayList;
+
 public class FileHandler {
+
+    private String fileName;
+
+    public FileHandler(String fileName){
+        this.fileName = fileName;
+    }
+
+    public void setFileName(String fileName){ this.fileName = fileName; }
+
+    public String getFileName(){ return fileName;}
+
+    public void saveRecords(ArrayList<DisplayableRecord> records)
+    {
+        /*Saves the ArrayList passed in to a file
+         with the file name specified by fileName using ObjectOutputStream*/
+
+    }
+
+    public ArrayList<DisplayableRecord> loadRecords()
+    {
+        /*Loads and returns an ArrayList from a file with the file name specified by fileName
+        using ObjectInputStream. Any exceptions thrown by the loading code should be caught
+        and handled in this method.
+         In the case of exceptions, an error message should be displayed and null returned.
+        * */
+
+        ArrayList<DisplayableRecord> records = new ArrayList<>();
+
+        return records;
+    }
+
+    public void exportReadableRecords( ArrayList<DisplayableRecord> records,
+                                       String readableFileName)
+    {
+        /* Writes the details of each record
+        to the specified file name in human-readable text format. */
+    }
 }
