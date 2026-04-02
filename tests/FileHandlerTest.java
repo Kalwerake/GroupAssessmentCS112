dfc27d76fc996d73f0dddbc567ccf316184849db
Kalwane Werake <35309973+Kalwerake@users.noreply.github.com>
2026-04-02 18:57:05 +0100

    filehandler test
    Co-authored-by: Kalwane Werake (Student) <kalwane.werake.2025@uni.strath.ac.uk>

---
+++ tests/FileHandlerTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class FileHandlerTest {
+
+    @org.junit.jupiter.api.Test
+    void setFileName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getFileName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void saveRecords() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void loadRecords() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void exportReadableRecords() {
+    }
+}
